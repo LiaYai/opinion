425381f24edb9f0fcdad2c26245a182777d1a42c
Talissa <liayai@ya.ru>
2025-02-14 21:04:10 +0300

    Updates

--- src/components/common/Loader.tsx
+++
@@ -1,5 +0,0 @@
-export const Loader = () => {
-  return (
-    <div className="w-6 h-6 border-4 border-gray-200 border-t-4 border-t-loader rounded-full animate-spin"></div>
-  );
-};
--- src/data/config.ts
+++
@@ -1,2 +0,0 @@
-export const BASE_PATH: string = import.meta.env.VITE_BASE_PATH || '/';
-export const ITEMS_PER_LOAD: number = 5;
