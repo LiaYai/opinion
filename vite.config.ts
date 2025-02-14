a67d2a74cd0b8cd9cba137d443da5830338d3c75
Talissa <liayai@ya.ru>
2025-02-14 19:49:50 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,5 @@ export default defineConfig({
       include: ['src/**/*.ts', 'src/**/*.tsx'],
     }),
   ],
-  base: env.NODE_ENV === 'production' ? '/opinion-gh/' : '/',
+  base: env.NODE_ENV === 'production' ? '/opinion/' : '/',
 });
